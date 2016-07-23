f84a42f496148e50b6b8532bf6cf5793ddccfd84
Ian Caffey <iancaffey@ymail.com>
2016-07-23 16:26:30 -0700

    General refactoring.

--- src/main/java/com/iancaffey/bytecode/attributes/StackFrame.java
+++ src/main/java/com/iancaffey/bytecode/attributes/StackFrame.java
@@ -7,11 +7,11 @@ package com.iancaffey.bytecode.attributes;
  * @since 1.0
  */
 public class StackFrame {
-    private final int type;
-    private final int offset;
     public static final int SINGLE_STACK_ITEM_FRAME_EXTENDED = 247;
     public static final int SAME_FRAME_EXTENDED = 251;
     public static final int FULL_FRAME = 255;
+    private final int type;
+    private final int offset;
 
     public StackFrame(int type, int offset) {
         this.type = type;
