aa7c2c507e912ff3c1429ad7b4c5e9a4b489732a
Ian Caffey <iancaffey@ymail.com>
2016-07-23 04:34:07 -0700

    Added convenience methods for checking various access masks for class/field/method.

---
+++ src/main/java/com/iancaffey/bytecode/util/Access.java
@@ -0,0 +1,45 @@
+package com.iancaffey.bytecode.util;
+
+import java.util.EnumSet;
+
+/**
+ * Access
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+public enum Access {
+    PUBLIC(0x0001),
+    PRIVATE(0x0002),
+    PROTECTED(0x0004),
+    STATIC(0x0008),
+    FINAL(0x0010),
+    INTERFACE(0x0200),
+    ABSTRACT(0x0400),
+    SYNTHETIC(0x1000),
+    ANNOTATION(0x2000),
+    ENUM(0x4000);
+    private final int mask;
+
+    Access(int mask) {
+        this.mask = mask;
+    }
+
+    public static EnumSet<Access> of(Accessible accessible) {
+        if (accessible == null)
+            throw new IllegalArgumentException();
+        return Access.of(accessible.access());
+    }
+
+    public static EnumSet<Access> of(int access) {
+        EnumSet<Access> set = EnumSet.noneOf(Access.class);
+        for (Access a : Access.values())
+            if (a.marked(access))
+                set.add(a);
+        return set;
+    }
+
+    public boolean marked(int access) {
+        return (mask & access) == mask;
+    }
+}
---
+++ src/main/java/com/iancaffey/bytecode/util/Accessible.java
@@ -0,0 +1,11 @@
+package com.iancaffey.bytecode.util;
+
+/**
+ * Accessible
+ *
+ * @author Ian Caffey
+ * @since 1.0
+ */
+public interface Accessible {
+    public int access();
+}
